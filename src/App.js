45f5a5c086a110ae4d2e7ba6a279f0a697ef467b
Hassaan016 <Hassaan016@github.com>
2020-06-12 16:43:36 +0500

    Updated App.js
    Added Muhammad Hassaan Shah string

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Muhammad Hassaan Shah
         </a>
       </header>
     </div>
